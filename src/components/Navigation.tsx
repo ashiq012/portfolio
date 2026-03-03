4a46a13e9a84ce8648d041b051ec2335e33df40d
Md Ashiq ilahi <128233331+ashiq012@users.noreply.github.com>
2026-03-03 16:02:35 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ export default function Navigation() {
     { name: 'About Me', href: '#about' },
     { name: 'Experience', href: '#experience' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Resume', href: 'https://drive.google.com/file/d/1sLu3ZuY9Zgj14idvSoYRFjc_tzA0sAXW/view?usp=sharing', external: true },
+    { name: 'Resume', href: 'https://drive.google.com/file/d/1Vx7_cyMkOG9YbpuRU2DOddKxw_leza6q/view?usp=drive_link', external: true },
   ];
 
   const menuVariants = {
