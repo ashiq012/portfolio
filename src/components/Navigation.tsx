4f48596e46666733ec1eb086cbdd599689dd9f5c
Md Ashiq ilahi <128233331+ashiq012@users.noreply.github.com>
2026-03-23 11:56:17 +0530

    Update Resume link in Navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ export default function Navigation() {
     { name: 'About Me', href: '#about' },
     { name: 'Experience', href: '#experience' },
     { name: 'Projects', href: '#projects' },
-    { name: 'Resume', href: 'https://drive.google.com/file/d/1DrVIwIcHrtOzSuKuwgeu2aY9Pzb-DW6Z/view?usp=drive_link', external: true },
+    { name: 'Resume', href: 'https://drive.google.com/file/d/1fQifhI8yuDkeMeYpakfdA9MmHzn6iUOi/view?usp=sharing', external: true },
   ];
 
   const menuVariants = {
